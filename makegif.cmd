3504899c91dbcbaa42228c189a9dff58e4ea36f9
Keijiro Takahashi <keijiro@gmail.com>
2016-11-18 20:10:09 +0900

    Forced to overwrite.

--- makegif.cmd
+++ makegif.cmd
@@ -1,3 +1,3 @@
-ffmpeg.exe -f image2 -r 24 -i screen-%%04d.tif -pix_fmt yuv420p -an video.mp4
-ffmpeg.exe -f image2 -r 24 -i screen-%%04d.tif -vf palettegen=max_colors=8 palette.png
-ffmpeg.exe -f image2 -r 24 -i screen-%%04d.tif -i palette.png -filter_complex "null[x];[x][1:v]paletteuse" -r 24 anim.gif
+ffmpeg.exe -f image2 -r 24 -i screen-%%04d.tif -y -pix_fmt yuv420p -an video.mp4
+ffmpeg.exe -f image2 -r 24 -i screen-%%04d.tif -y -vf palettegen=max_colors=8 palette.png
+ffmpeg.exe -f image2 -r 24 -i screen-%%04d.tif -y -i palette.png -filter_complex "null[x];[x][1:v]paletteuse" -r 24 anim.gif
